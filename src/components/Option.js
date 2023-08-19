afbf0625e19c435fbfb3f5a3482c64e7333ef84c
alexandrudaniel3 <alexandrudanielbortos@gmail.com>
2023-08-19 20:28:28 +0300

    styling

--- src/components/Option.js
+++ src/components/Option.js
@@ -2,12 +2,9 @@ import React from "react";
 import './styles/Option.css';
 
 export default function Option({optionKey, optionValue, isSelected, clickHandler}) {
-    const buttonStyle = {
-        backgroundColor: isSelected ? 'lightgray' : 'white'
-    };
     return (
         <div className='option-container'>
-            <button className='option' style={buttonStyle} onClick={(e) => clickHandler(optionKey)}>
+            <button className={isSelected ? 'option chosen-option' : 'option'} onClick={(e) => clickHandler(optionKey)}>
                     <p className='option-key'>{optionKey}</p>
                     <p className='option-value'>{optionValue}</p>
             </button>
